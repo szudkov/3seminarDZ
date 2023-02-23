a0891f070786cc34ecc712c313139f36c444494c
Sergy <zudkov@ya.ru>
2023-02-23 21:24:55 +0300

    palindrom

---
+++ palindrom/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+int x = number/10000;
+int y = number%10;
+if (x == y)
+{
+    Console.WriteLine("Это палиндром");
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
